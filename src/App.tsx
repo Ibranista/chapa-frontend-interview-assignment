35441321a1760f5efa99796aa29e69ff3f3ac8df
Ibrahim Kedir <techofreact@gmail.com>
2025-07-09 15:57:25 +0300

    ft: basic welcome page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
+import { useEffect } from "react";
+import { selectUser } from "./features/selectors/user.selector";
+import { useDispatch, useSelector } from "react-redux";
+import type { AppDispatch } from "./store/store";
+import { fetchBalance } from "./features/thunk/recentTransactions.thunk";
+
 function App() {
+  const dispatch = useDispatch<AppDispatch>();
+  const user = useSelector(selectUser)?.user;
+
+  useEffect(() => {
+    if (user?.id) {
+      dispatch(fetchBalance(user.id));
+    }
+  }, [dispatch, user?.id]);
+
+  if (!user?.id) return <div>Loading...</div>;
+
   return (
-    <>
-      <h1 className="text-3xl font-bold underline text-orange-200">
-        hello world!
-      </h1>
-    </>
+    <div className="dashboard-section">
+      <div>Welcome, {user?.name} ({user?.role})</div>
+    </div>
   );
 }
 
