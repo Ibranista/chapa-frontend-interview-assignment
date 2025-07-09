a5a50fa00b65d54c9fdc5e8c519612ef0f528cfd
Ibrahim Kedir <techofreact@gmail.com>
2025-07-09 16:01:43 +0300

    ft: store manager for redux.

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,6 +1,18 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import authReducer from "@/features/slice/auth.slice";
+import usersReducer from "@/features/slice/users.slice";
+import transactionSummaryReducer from "@/features/slice/transaction.slice";
+import adminReducer from "@/features/slice/management.slice";
+import recentTransactionsReducer from "@/features/slice/recentTransactions.slice";
 
-const rootReducer = combineReducers({});
+const rootReducer = combineReducers({
+  auth: authReducer,
+  users: usersReducer,
+  transactionSummary: transactionSummaryReducer,
+  admin: adminReducer,
+  recentTransactions: recentTransactionsReducer,
+});
+
 
 export const store = configureStore({
   reducer: rootReducer,
