ac9939cd37fe0341506ed7429ffabac5cfd3a883
Ibrahim Kedir <techofreact@gmail.com>
2025-07-09 15:57:49 +0300

    ft: initiate worker for the api mocker and provide chakra,auth, and router.

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,27 @@
+import "./index.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import "./index.css";
 import { store } from "@/store/store.ts";
 import { RouterProvider } from "react-router-dom";
 import { router } from "@/routes/index.tsx";
+import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
+import AuthProvider from "./components/AuthProvider";
+
+if (import.meta.env.MODE === 'development') {
+  console.log("mocked")
+  const { worker } = await import('./api/browser');
+  worker.start();
+}
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <AuthProvider>
+        <ChakraProvider value={defaultSystem}>
+          <RouterProvider router={router} />
+        </ChakraProvider>
+      </AuthProvider>
     </Provider>
   </StrictMode>
 );
