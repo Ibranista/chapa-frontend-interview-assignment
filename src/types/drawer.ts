cf1a135af4cd2f218c631e96a38d6d2212a61d9f
Ibrahim Kedir <techofreact@gmail.com>
2025-07-09 17:45:29 +0300

    refactor: remove chakra related libraries.

--- src/types/drawer.ts
+++
@@ -1,7 +0,0 @@
-import type * as ChakraDrawer from "@chakra-ui/react";
-
-export interface DrawerContentProps extends ChakraDrawer.ContentProps {
-  portalled?: boolean;
-  portalRef?: React.RefObject<HTMLElement | null>;
-  offset?: ChakraDrawer.ContentProps["padding"];
-} 
